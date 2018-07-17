a777cd09c186ebb3c36b1959c31022f907080540
Yishai Wiesner <wyishai@gmail.com>
2018-07-17 08:45:47 +0300

    Add unittest for the clean operation

--- tests/gtfspy/test_transit_data_object.py
+++ tests/gtfspy/test_transit_data_object.py
@@ -31,6 +31,24 @@ class TestTransitDataLoad(unittest.TestCase):
             if os.path.exists(temp_file_path):
                 os.remove(temp_file_path)
 
+    def test_clean(self):
+        td = create_full_transit_data()
+        for trip in td.trips:
+            for stop_time in list(trip.stop_times):
+                stop_time.trip.stop_times.remove(stop_time)
+                stop_time.stop.stop_times.remove(stop_time)
+
+        td.clean()
+
+        self.assertEqual(0, len(td.agencies))
+        self.assertEqual(0, len(td.routes))
+        self.assertEqual(0, len(td.trips))
+        self.assertEqual(0, len(td.shapes))
+        self.assertEqual(0, len(td.calendar))
+        self.assertEqual(0, len(td.stops))
+        self.assertEqual(0, len(td.fare_attributes))
+        self.assertEqual(0, len(td.fare_rules))
+
 
 if __name__ == '__main__':
     unittest.main()
